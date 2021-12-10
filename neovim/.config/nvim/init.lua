d03c33f65b8f3b2fabac80de6651cff838bca886
Pierre <pierre.git@posteo.de>
2021-12-11 00:06:05 +0100

    [neovim] treesitter playground

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -24,6 +24,7 @@ require('packer').startup(function()
   use 'rhysd/vim-clang-format'
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'nvim-treesitter/playground'
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use 'joshdick/onedark.vim'
   use 'mbbill/undotree'
@@ -158,6 +159,24 @@ vim.api.nvim_set_keymap('n', '<leader>0', ':nohl<cr>', { noremap = true, silent 
 
 -- Treesitter configuration
 require('nvim-treesitter.configs').setup {
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
+  },
   highlight = {
     enable = true, -- false will disable the whole extension
   },
