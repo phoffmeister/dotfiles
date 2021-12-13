d5a98d3bfa7cca29d8f2c56e9597388ed20fc89e
Pierre <pierre.git@posteo.de>
2021-12-13 14:04:43 +0100

    [neovim] enable tab for completion

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -329,10 +329,34 @@ require"lspconfig".efm.setup {
 }
 
 -- nvim-cmp setup
+local luasnip = require 'luasnip'
 local cmp = require 'cmp'
 cmp.setup {
-    snippet = {expand = function(args) require('luasnip').lsp_expand(args.body) end},
+    snippet = {
+        expand = function(args) luasnip.lsp_expand(args.body) end
+    },
     mapping = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
