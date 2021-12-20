7107186b20a912644586a1fa178ab0fcb691cde4
Pierre <pierre.git@posteo.de>
2021-12-20 22:17:16 +0100

    [neovim] configure completion

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -30,14 +30,16 @@ require('packer').startup(function()
     use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}}
     use 'joshdick/onedark.vim'
     use 'mbbill/undotree'
+    use 'onsails/lspkind-nvim'
     use 'neovim/nvim-lspconfig'
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-nvim-lua'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/vim-vsnip'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'L3MON4D3/LuaSnip'
 
     use {
         'nvim-lualine/lualine.nvim',
@@ -45,8 +47,6 @@ require('packer').startup(function()
     }
     use 'lukas-reineke/indent-blankline.nvim'
     use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
 end)
 
 vim.o.termguicolors = true
@@ -89,20 +89,12 @@ vim.o.updatetime = 50
 vim.o.spelllang = 'en'
 vim.o.shortmess = vim.o.shortmess .. 'c'
 
-local function trailing()
-  local space = vim.fn.search([[\s\+$]], 'nwc')
-  return space ~= 0 and "TW:"..space or ""
-end
-
 require'lualine'.setup({
     options = {
         theme = 'onedark',
         section_separators = {left = '┊', right = '┊'},
         component_separators = {left = '┊', right = '┊'}
     },
-    sections = {
-        lualine_z = { trailing },
-    }
 })
 
 -- comment.nvim
@@ -201,7 +193,7 @@ vim.api.nvim_set_keymap('n', '<leader>0', ':nohl<cr>',
 -- Treesitter configuration
 require('nvim-treesitter.configs').setup {
     playground = {
-        enable = true,
+        enable = false,
         disable = {},
         updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
         persist_queries = false, -- Whether the query persists across vim sessions
@@ -337,48 +329,45 @@ require"lspconfig".efm.setup {
 }
 
 -- nvim-cmp setup
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-local luasnip = require 'luasnip'
-local cmp = require 'cmp'
-cmp.setup {
+local lspkind = require'lspkind'
+local cmp = require'cmp'
+cmp.setup({
     snippet = {
-        expand = function(args) luasnip.lsp_expand(args.body) end
+        expand = function(args)
+		require('luasnip').lsp_expand(args.body)
+	end,
     },
     mapping = {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, {"i", "s"}),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, {"i", "s"}),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
+        ['<C-y>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
-        }
+        },
+        ['<C-Space>'] = cmp.mapping.complete(),
     },
-    sources = {{name = 'nvim_lsp'}, {name = 'luasnip'}}
-}
+    sources = {
+        {name = 'nvim_lua'},
+        {name = 'nvim_lsp'},
+        {name = 'path'},
+        {name = 'luasnip'},
+        {name = 'buffer', keyword_length = 5},
+    },
+    formatting = {
+        format = lspkind.cmp_format({
+            with_text = true,
+            menu = {
+                buffer = "[buf]",
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[api]",
+                path = "[path]",
+                luasnip = "[snip]",
+            }
+        })
+    },
+    experimental = {
+        native_menu = false,
+        ghost_text = true
+    }
+})
