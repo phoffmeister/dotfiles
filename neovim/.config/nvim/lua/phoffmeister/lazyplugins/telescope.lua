10c5d312c4f6327e2d00ce6428eb8c17ed74f4f6
Pierre Hoffmeister <pierre.hoffmeister@trivago.com>
2024-02-15 14:28:52 +0100

    why was this ever different

--- neovim/.config/nvim/lua/phoffmeister/lazyplugins/telescope.lua
+++ neovim/.config/nvim/lua/phoffmeister/lazyplugins/telescope.lua
@@ -1,12 +1,13 @@
 return {
     'nvim-lua/plenary.nvim',
     {
-    'nvim-telescope/telescope.nvim',
+        'nvim-telescope/telescope.nvim',
         config = function()
             require('telescope').setup {
                 defaults = {
-                    mappings = { i = { ['<C-u>'] = false, ['<C-d>'] = false } },
+                    layout_strategy = 'vertical',
                     file_ignore_patterns = { ".git", "node_modules" },
+                    path_display = { "shorten" },
                 }
             }
 
